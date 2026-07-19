941f04b44df40e9bc69853eadba4b9900d435240
NikhByte <nikhil010407@gmail.com>
2026-07-19 14:27:20 +0530

    added list of compilers,browsers etc for the optimisation

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "cognios_telemetry.db")
 SLIDING_WIND_N = 120
+COMPILERS = ["gcc", "g++", "clang", "make", "ninja", "rustc", "javac"]
+BROWSERS = ["chrome", "firefox", "brave", "msedge"]
+CALLS = ["zoom", "teams", "slack", "discord", "skype", "webex"]
+IDES = ["code", "code-insiders", "sublime", "vim", "nvim"]
