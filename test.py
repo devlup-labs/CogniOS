552964ff685a28c5441e10b98ba818bc451c6942
Abhishek Reddy <nabhishekreddy22@gmail.com>
2026-07-21 11:57:57 +0530

    Pre-processing improved in i_forest.py

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from os_doctor.os_doctor_db import execute_os_doctor_db
 from cognios_as_daemon import run_daemon
 
 if __name__ == "__main__":
-    threading.Thread(target=execute_os_doctor_db, daemon=True).start()
+    threading.Thread(target=execute_os_doctor_db, daemon=True).start() 
     run_daemon()
